fa0de71a2faec6051a37604a44128a6114bee9be
ruveydakisla <ruveydakisla34@gmail.com>
2025-03-05 10:56:58 +0300

    linear gradient background hook added and used

--- src/app/player.tsx
+++ src/app/player.tsx
@@ -5,8 +5,10 @@ import { PlayerRepeatToggle } from '@/components/PlayerRepeatToggle'
 import { PlayerVolumeBar } from '@/components/PlayerVolumeBar'
 import { unknownTrackImageUri } from '@/constants/images'
 import { colors, fontSize, screenPadding } from '@/constants/tokens'
+import { usePlayerBackground } from '@/hooks/usePlayerBackground'
 import { defaultStyles, utilStyles } from '@/styles'
 import { FontAwesome } from '@expo/vector-icons'
+import { LinearGradient } from 'expo-linear-gradient'
 import React from 'react'
 import { ActivityIndicator, StyleSheet, Text, View } from 'react-native'
 import FastImage from 'react-native-fast-image'
@@ -15,6 +17,7 @@ import { useActiveTrack } from 'react-native-track-player'
 
 const PlayerScreen = () => {
 	const activeTrack = useActiveTrack()
+	const { imageColors } = usePlayerBackground(activeTrack?.artwork ?? unknownTrackImageUri)
 	const { top, bottom } = useSafeAreaInsets()
 	const isFavorite = true
 
@@ -25,71 +28,80 @@ const PlayerScreen = () => {
 		</View>
 	}
 	return (
-		<View style={styles.overlayContainer}>
-			<DissmissPlayerSymbol />
+		<LinearGradient
+			style={{ flex: 1 }}
+			colors={
+				imageColors
+					? [imageColors.background, imageColors.primary]
+					: [colors.background, colors.background]
+			}
+		>
+			<View style={styles.overlayContainer}>
+				<DissmissPlayerSymbol />
 
-			<View style={{ flex: 1, marginTop: top + 70, marginBottom: bottom }}>
-				<View style={styles.artWorkImageContainer}>
-					<FastImage
-						source={{
-							uri: activeTrack?.artwork ?? unknownTrackImageUri,
-							priority: FastImage.priority.high,
-						}}
-						resizeMode="cover"
-						style={styles.artworkImage}
-					/>
-				</View>
+				<View style={{ flex: 1, marginTop: top + 70, marginBottom: bottom }}>
+					<View style={styles.artWorkImageContainer}>
+						<FastImage
+							source={{
+								uri: activeTrack?.artwork ?? unknownTrackImageUri,
+								priority: FastImage.priority.high,
+							}}
+							resizeMode="cover"
+							style={styles.artworkImage}
+						/>
+					</View>
 
-				<View style={{ flex: 1 }}>
-					<View style={{ marginTop: 'auto' }}>
-						<View style={{ height: 60 }}>
-							<View
-								style={{
-									flexDirection: 'row',
-									justifyContent: 'space-between',
-									alignItems: 'center',
-								}}
-							>
-								{/* Track title */}
-								<View style={styles.trackTitleContainer}>
-									<MovingText
-										text={activeTrack?.title ?? ''}
-										animationThreshold={30}
-										style={styles.trackTitleText}
+					<View style={{ flex: 1 }}>
+						<View style={{ marginTop: 'auto' }}>
+							<View style={{ height: 60 }}>
+								<View
+									style={{
+										flexDirection: 'row',
+										justifyContent: 'space-between',
+										alignItems: 'center',
+									}}
+								>
+									{/* Track title */}
+									<View style={styles.trackTitleContainer}>
+										<MovingText
+											text={activeTrack?.title ?? ''}
+											animationThreshold={30}
+											style={styles.trackTitleText}
+										/>
+									</View>
+
+									{/* Favorite button icon */}
+									<FontAwesome
+										name={isFavorite ? 'heart' : 'heart-o'}
+										size={20}
+										color={isFavorite ? colors.primary : colors.icon}
+										style={{ marginHorizontal: 14 }}
+										onPress={toggleFavorite}
 									/>
 								</View>
 
-								{/* Favorite button icon */}
-								<FontAwesome
-									name={isFavorite ? 'heart' : 'heart-o'}
-									size={20}
-									color={isFavorite ? colors.primary : colors.icon}
-									style={{ marginHorizontal: 14 }}
-									onPress={toggleFavorite}
-								/>
+								{/* Track artist */}
+								{activeTrack?.artist && (
+									<Text numberOfLines={1} style={[styles.trackArtistText, { marginTop: 6 }]}>
+										{activeTrack.artist}
+									</Text>
+								)}
 							</View>
 
-							{/* Track artist */}
-							{activeTrack?.artist && (
-								<Text numberOfLines={1} style={[styles.trackArtistText, { marginTop: 6 }]}>
-									{activeTrack.artist}
-								</Text>
-							)}
+							<PlayerProgressBar style={{ marginTop: 32 }} />
+
+							<PlayerControls style={{ marginTop: 40 }} />
 						</View>
 
-						<PlayerProgressBar style={{ marginTop: 32 }} />
+						<PlayerVolumeBar style={{ marginTop: 'auto', marginBottom: 30 }} />
 
-						<PlayerControls style={{ marginTop: 40 }} />
-					</View>
-
-					<PlayerVolumeBar style={{ marginTop: 'auto', marginBottom: 30 }} />
-
-					<View style={utilStyles.centeredRow}>
-						<PlayerRepeatToggle size={30} style={{ marginBottom: 6 }} />
+						<View style={utilStyles.centeredRow}>
+							<PlayerRepeatToggle size={30} style={{ marginBottom: 6 }} />
+						</View>
 					</View>
 				</View>
 			</View>
-		</View>
+		</LinearGradient>
 	)
 }
 
